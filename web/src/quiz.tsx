02c9ccaeea5fb9dd95cdfce7a3173f70eeab6e39
huuthien <kevinnguyen31415@gmail.com>
2023-11-04 21:35:14 +1100

    init

--- web/src/quiz.tsx
+++ web/src/quiz.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const Quiz = () => {
+  const params = useParams()
+  const [quiz, setQuiz] = useState()
+
+  useEffect(() => {
+
+  })
   return (
-    <div>Quiz</div>
+    <div>{params.id}</div>
   )
 }
